d8b4e174f33b585fff18a4dbc7b2cb598c882160
ryan <ryan@jembi.org>
2014-04-04 17:22:34 +0200

    Added a channels view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/channels', {
+        templateUrl: 'views/channels.html',
+        controller: 'ChannelsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/channels.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('openhimWebui2App')
+  .controller('ChannelsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
