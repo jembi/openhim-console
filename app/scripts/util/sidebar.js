95dea27636b6d1340686cd2d32519d9db7b73ae7
ryan <ryan@jembi.org>
2014-05-06 15:51:20 +0200

    Fixed ngInclude to work in grunt dist build of app, fixed sidebar script to work with usemin

---
+++ app/scripts/util/sidebar.js
@@ -0,0 +1,18 @@
+'use strict';
+/* global $:false */
+/* exported toggleSubMenu */
+
+function toggleSubMenu(element) {
+  var $li = $(element).parent('li');
+  var $ul = $(element).next('ul');
+
+  if($li.hasClass('open')) {
+    $ul.slideUp(350);
+    $li.removeClass('open');
+  } else {
+    $('.nav > li > ul').slideUp(350);
+    $('.nav > li').removeClass('open');
+    $ul.slideDown(350);
+    $li.addClass('open');
+  }
+}
