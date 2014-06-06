fbb3ec1fdee76954837a3bb28ea63679ee1df5be
devcritter <venterh@gmail.com>
2014-06-06 16:08:08 +0200

    Added client tests

--- test/spec/controllers/clientsmodal.js
+++ test/spec/controllers/clientsmodal.js
@@ -1,19 +1,56 @@
 'use strict';
+/* global sinon:false */
+/* jshint expr: true */
 
-describe('Controller: ClientsModalCtrl', function () {
+describe('Controller: ClientsmodalCtrl', function () {
 
   // load the controller's module
   beforeEach(module('openhimWebui2App'));
 
-  var ClientsModalCtrl,
-    scope;
+  var scope, createController;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    ClientsModalCtrl = $controller('ClientsModalCtrl', {
-      $scope: scope
-    });
+    var modalInstance = sinon.spy();
+
+    createController = function (withClient) {
+      var client;
+      if (withClient) {
+        client = {
+          $save: sinon.spy(),
+          $update: sinon.spy()
+        };
+      }
+      return $controller('ClientsModalCtrl', {
+        $scope: scope,
+        $modalInstance: modalInstance,
+        client: client
+      });
+    };
+
   }));
 
+  it('should create a new client if this is not an update', function () {
+    createController();
+    scope.client.should.be.ok;
+  });
+
+  it('should save a new client', function () {
+    createController();
+    sinon.spy(scope.client, '$save');
+    scope.save(scope.client);
+    scope.client.$save.should.be.called;
+  });
+
+  it('should set a client to be updated if one is supplied', function () {
+    createController(true);
+    scope.client.should.be.ok;
+  });
+
+  it('should update an existing client', function () {
+    createController(true);
+    scope.save(scope.client);
+    scope.client.$update.should.be.called;
+  });
 });
