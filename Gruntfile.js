5fd745de1bf6d034876bbc6f0bba982b5e998b07
devcritter <venterh@gmail.com>
2014-06-09 12:40:22 +0200

    Exclude bcrypt and crytojslib from bowerInstall.

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,6 +147,7 @@ module.exports = function (grunt) {
     bowerInstall: {
       app: {
         src: ['<%= yeoman.app %>/index.html'],
+        exclude: ['/cryptojslib', 'bcrypt'],
         ignorePath: '<%= yeoman.app %>/'
       }
     },
