424078a151e0072eebd07a8da3319748fd442b5d
Martin Brocker <martinbrocker@yahoo.com>
2014-06-11 15:26:02 +0200

    Started working on login page

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,7 +3,7 @@
 angular.module('openhimWebui2App')
   .controller('LoginCtrl', function ($scope, login, $window) {
     // temporarily hard code this: (ie. force root user to be logged in)
-    login.login('root@openhim.org', 'openhim-password', function(loggedIn) {
+    login.login('root@openhim.orgaa', 'openhim-password', function(loggedIn) {
       if (loggedIn) {
         console.log('Logged in, redirecting...');
         $window.location = '#/channels';
