456cb22ff541dcf2bafeaca333685fef36455ca0
Duma Mtungwa <duma@jembi.org>
2014-06-17 16:03:40 +0200

    added profile page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -47,6 +47,10 @@ angular
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
       })
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('openhimWebui2App')
+  .controller('ProfileCtrl', function ($scope) {
+    $scope.setSomething = true;
+  });
