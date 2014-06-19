0b7c1bfe8c7548eec097ad6145aea83b85f4eb57
Duma Mtungwa <duma@jembi.org>
2014-06-19 14:39:54 +0200

    added user profile page

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,6 +1,45 @@
 'use strict';
 
 angular.module('openhimWebui2App')
-  .controller('ProfileCtrl', function ($scope) {
-    $scope.setSomething = true;
+  .controller('ProfileCtrl', function ($scope, login, Api, Notify) {
+    var update = true;
+    $scope.user = login.getLoggedInUser();
+
+    var done = function () {
+      // reset backing object and refresh users list
+      $scope.user = new Api.Users();
+      Notify.notify('usersChanged');
+
+
+    };
+
+    var saveUser = function (user) {
+      if (update) {
+        user.$update(done);
+      } else {
+        user.$save({ email: '' }, done);
+      }
+    };
+
+    var setHashAndSave = function (user, hash, salt) {
+      if (typeof salt !== 'undefined' && salt !== null) {
+        user.passwordSalt = salt;
+      }
+      user.passwordHash = hash;
+      saveUser(user);
+    };
+
+    $scope.save = function (user, password) {
+      if (password) {
+        var h = getHashAndSalt(password);
+        user.passwordAlgorithm = h.algorithm;
+        setHashAndSave(user, h.hash, h.salt);
+      } else {
+        saveUser(user);
+      }
+    };
+
+    $scope.isUserValid = function (user, password, passwordRetype) {
+      return  !(password && password !== passwordRetype);
+    };
   });
--- app/scripts/services/login.js
+++ app/scripts/services/login.js
@@ -6,36 +6,45 @@ angular.module('openhimWebui2App')
 
     var username = null;
     var passwordhash = null;
+    var firstname = null;
+    var surname = null;
+    var groups = null;
 
     return {
       login: function (email, password, done) {
         // fetch salt from openhim-core serer and work out password hash
         Api.Authenticate.get({ email: email }, function (authDetails) {
-          // on success
-          if (!authDetails.salt) {
+            // on success
+            if (!authDetails.salt) {
+              done(false);
+            }
+
+            var sha512 = CryptoJS.algo.SHA512.create();
+            sha512.update(authDetails.salt);
+            sha512.update(password);
+            var hash = sha512.finalize();
+
+            passwordhash = hash.toString(CryptoJS.enc.Hex);
+            username = email;
+            // Fetch currently logged in user's profile
+            Api.Users.get({ email: email }, function (userProfile) {
+              firstname = userProfile.firstname;
+              surname = userProfile.surname;
+              groups = userProfile.groups;
+            });
+
+            // notify the authInterceptor of a logged in user
+            Authinterceptor.setLoggedInUser({
+              username: username,
+              passwordhash: passwordhash
+            });
+
+            done(true);
+          },
+          function () {
+            // on error
             done(false);
-          }
-
-          var sha512 = CryptoJS.algo.SHA512.create();
-          sha512.update(authDetails.salt);
-          sha512.update(password);
-          var hash = sha512.finalize();
-
-          passwordhash = hash.toString(CryptoJS.enc.Hex);
-          username = email;
-
-          // notify the authInterceptor of a logged in user
-          Authinterceptor.setLoggedInUser({
-            username: username,
-            passwordhash: passwordhash
           });
-
-          done(true);
-        },
-        function () {
-          // on error
-          done(false);
-        });
       },
       logout: function () {
         username = null;
@@ -44,7 +53,11 @@ angular.module('openhimWebui2App')
       getLoggedInUser: function () {
         return {
           username: username,
-          passwordhash: passwordhash
+          passwordhash: passwordhash,
+          firstname: firstname,
+          surname: surname,
+          groups: groups
+
         };
       },
       isLoggedIn: function () {
