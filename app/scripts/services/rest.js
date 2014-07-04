5f4670fe8d4939678ac2644e3660a31a82a8fed0
Martin Brocker <martinbrocker@yahoo.com>
2014-07-04 10:53:23 +0200

    uploading rest.js file

---
+++ app/scripts/services/rest.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('openhimWebui2App')
+  .factory('Api', function ($resource) {
+
+    var host = 'openhim-preprod.jembi.org';
+    var port = '8080';
+
+    return {
+      Authenticate: $resource('http://' + host + ':' + port + '/authenticate/:email'),
+
+      Channels: $resource('http://' + host + ':' + port + '/channels/:channelName', { channelName: '@name' }, {
+        update: { method: 'PUT' }
+      }),
+
+      Users: $resource('http://' + host + ':' + port + '/users/:email', { email: '@email' }, {
+        update: { method: 'PUT' }
+      }),
+
+      Clients: $resource('http://' + host + ':' + port + '/clients/:clientID', { clientID: '@clientID' }, {
+        update: { method: 'PUT' }
+      }),
+
+      Transactions: $resource('http://' + host + ':' + port + '/transactions/:transactionId', { transactionId: '@_id' }),
+
+      Tasks: $resource('http://' + host + ':' + port + '/tasks/:taskId', { taskId: '@_id' }, {
+        update: { method: 'PUT' }
+      }),
+      Queue: $resource('http://' + host + ':' + port + '/queues/:queueId', { queueId: '@_id' })
+    };
+  });
