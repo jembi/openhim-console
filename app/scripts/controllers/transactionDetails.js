3d901410755118fcb78b505f9772877a75e946dd
Martin Brocker <martinbrocker@yahoo.com>
2014-07-04 08:42:17 +0200

    Updated transactions view for child transactions

--- app/scripts/controllers/transactionDetails.js
+++ app/scripts/controllers/transactionDetails.js
@@ -1,10 +1,53 @@
 'use strict';
 
 angular.module('openhimWebui2App')
-  .controller('TransactionDetailsCtrl', function ($scope, $modal, $routeParams, Api) {
+  .controller('TransactionDetailsCtrl', function ($scope, $modal, $location, $routeParams, Api) {
 
     //get the Data for the supplied ID and store in 'transactionsDetails' object
     $scope.transactionDetails = Api.Transactions.get({ transactionId: $routeParams.transactionId });
+
+    /*------------------------Transactions List and Detail view functions----------------------------*/
+    //setup filter options
+    $scope.returnFilterObject = function(){
+      var filtersObject = {};
+      
+      filtersObject.filterPage = 0;
+      filtersObject.filterLimit = 9999999;
+      filtersObject.parentID = $routeParams.transactionId;
+      return filtersObject;
+    };
+
+    //Refresh transactions list
+    $scope.fetchChildTransactions = function () {
+
+      Api.Transactions.query( $scope.returnFilterObject(), function (values) {
+        // on success
+        $scope.childTransactions = values;
+
+        if( values.length < 1 ){
+          
+        }else{
+          
+        }
+
+      },
+      function (err) {
+        // on error - Hide load more button and show error message
+        $scope.returnError(err.status);
+      });
+
+    };
+    //run the transaction list view for the first time
+    $scope.fetchChildTransactions();
+
+
+    //location provider - load transaction details
+    $scope.viewTransactionDetails = function (path) {
+      //do transactions details redirection when clicked on TD
+      $location.path(path);
+    };    
+    /*------------------------Transactions List and Detail view functions----------------------------*/
+
 
     /*------------------------Transactions ReRun Functions----------------------------*/
     $scope.confirmRerunTransactions = function(){
