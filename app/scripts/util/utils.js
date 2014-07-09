eb3b1f0206bfd5631865be967fa11d67ea46492b
Duma Mtungwa <duma@jembi.org>
2014-07-09 11:18:04 +0200

    fixed issue with user login - Swapped order of salt and password

--- app/scripts/util/utils.js
+++ app/scripts/util/utils.js
@@ -5,8 +5,8 @@
 function getHashAndSalt(stringToBeHashed) {
   var salt = CryptoJS.lib.WordArray.random(16).toString();
   var sha512 = CryptoJS.algo.SHA512.create();
-  sha512.update(stringToBeHashed);
   sha512.update(salt);
+  sha512.update(stringToBeHashed);
   var hash = sha512.finalize();
   return  {
     hash: hash.toString(CryptoJS.enc.Hex),
