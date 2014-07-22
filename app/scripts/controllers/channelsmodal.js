ba0a4fe4d493ded84f9f377136e4aac0542b1063
Martin Brocker <martinbrocker@yahoo.com>
2014-07-22 14:13:15 +0200

    Updated channels modal form with minor fixes

--- app/scripts/controllers/channelsmodal.js
+++ app/scripts/controllers/channelsmodal.js
@@ -2,6 +2,8 @@
 
 angular.module('openhimWebui2App')
   .controller('ChannelsModalCtrl', function ($scope, $modalInstance, Api, Notify, channel) {
+    
+    $scope.routeWarnings = [];
     var update = false;
     $scope.contentMatching = 'No matching';
     if (channel) {
@@ -64,9 +66,13 @@ angular.module('openhimWebui2App')
       }
       $scope.channel.routes.push(angular.copy(newRoute));
 
+      // Check if any route warnings exist and add them to routeWarnings object
+      $scope.checkRouteWarnings();
+
       // reset the backing object
       $scope.newRoute.name = null;
       $scope.newRoute.path = null;
+      $scope.newRoute.pathTransform = null;
       $scope.newRoute.host = null;
       $scope.newRoute.port = null;
       $scope.newRoute.username = null;
@@ -77,11 +83,45 @@ angular.module('openhimWebui2App')
     $scope.editRoute = function (routeIndex, route) {
       $scope.channel.routes.splice(routeIndex, 1);
       $scope.newRoute = route;
+
+      // Check if any route warnings exist and add them to routeWarnings object
+      $scope.checkRouteWarnings();
     };
 
     $scope.removeRoute = function (routeIndex) {
       $scope.channel.routes.splice(routeIndex, 1);
+
+      // Check if any route warnings exist and add them to routeWarnings object
+      $scope.checkRouteWarnings();
     };
+
+    $scope.checkRouteWarnings = function () {
+
+      // clear the routeWarnings object to have a clean object
+      $scope.routeWarnings = [];
+      var countErrors = 0;
+
+      if ( $scope.noRoutes() == true ){ $scope.routeWarnings.push('You must supply atleast one route.'), countErrors++; }
+      if ( $scope.noPrimaries() == true ){ $scope.routeWarnings.push('Atleast one of your routes must be set to the primary.'), countErrors++; }
+      if ( $scope.multiplePrimaries() == true ){ $scope.routeWarnings.push('You cannot have multiple primary routes.'), countErrors++; }
+
+      return countErrors;
+      
+    }
+
+
+    // verify if any warnings exist - if warnings exist then disable channel save button
+    $scope.checkChannelWarnings = function () {
+
+      var routeWarnings = $scope.checkRouteWarnings();
+
+      if ( routeWarnings > 0 ){
+        return true;
+      }
+      return false
+ 
+    }
+
 
     $scope.multiplePrimaries = function () {
       if ($scope.channel.routes) {
@@ -101,37 +141,26 @@ angular.module('openhimWebui2App')
       return false;
     };
 
-
-
     $scope.noPrimaries = function () {
-      if ($scope.channel.routes) {
-        var routes = $scope.channel.routes;
-        var count = 0;
-        for (var i = 0 ; i < routes.length ; i++) {
-          if (routes[i].primary === true) {
-            count++;
-          }
-
-          if (count == 0) {
-            return true;
+      if ($scope.channel.routes.length > 0) {
+        for (var i = 0 ; i < $scope.channel.routes.length ; i++) {
+          if ($scope.channel.routes[i].primary === true) {
+            // atleast one primary so return false
+            return false;
           }
         }
       }
-
-      return false;
+      // return true if no primary routes found
+      return true;
     };
 
 
     /* ------------------ Check to see if routes are empty --------------------- */
     $scope.noRoutes = function () {
-      if ($scope.channel.routes) {
-        var routes = $scope.channel.routes;
-        //no routes found - return true
-        if( routes.length == 0 ){
-          return true
-        }
+      //no routes found - return true
+      if( $scope.channel.routes.length == 0 ){
+        return true
       }
-
       return false;
     };
     /* ------------------ Check to see if routes are empty --------------------- */
