02e00545214befca9d80ce360fd860c5a1d49977
Duma Mtungwa <duma@jembi.org>
2014-07-22 15:12:02 +0200

    fixes issue #48

--- app/scripts/services/authinterceptor.js
+++ app/scripts/services/authinterceptor.js
@@ -24,13 +24,29 @@ angular.module('openhimWebui2App')
           var passwordhash = user.passwordHash;
           var requestSalt = CryptoJS.lib.WordArray.random(16).toString();
           var requestTS = new Date().toISOString();
-          var username = user.username || user.email;
+          try {
+            /**
+             * Try and syncronize with server time
+             *
+             */
+           requestTS = new Date(Math.abs(new Date().getTime() + user.timeDiff)).toISOString();
+          } catch (e) {
+            console.log(e.message);
+          }
+          var username = user.email;
 
           var sha512 = CryptoJS.algo.SHA512.create();
           sha512.update(passwordhash);
           sha512.update(requestSalt);
           sha512.update(requestTS);
           var hash = sha512.finalize();
+
+          /**
+           * This console.log has been left here so that you can view the time diff between your client and the server
+           * adjust client accordingly to test.
+           * This will be removed when the code is merged into master
+           */
+          console.log('timeDiff: ' + user.timeDiff);
 
           config.headers['auth-username'] = username;
           config.headers['auth-ts'] = requestTS;
