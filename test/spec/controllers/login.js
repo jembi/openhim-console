7192d20027fcaddcb85f1e25d30f0169a651cb54
Martin Brocker <martinbrocker@yahoo.com>
2014-07-23 14:11:21 +0200

    Minor text change

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -3,7 +3,7 @@
 
 describe('Controller: LoginCtrl', function () {
 
-  // load the service's module
+  // load the controller's module
   beforeEach(module('openhimWebui2App'));
 
   // instantiate service
