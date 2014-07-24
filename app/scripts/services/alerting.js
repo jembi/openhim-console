761dd2dd920813db6e4b84fb7c25fc03cdef51b8
Martin Brocker <martinbrocker@yahoo.com>
2014-07-24 10:23:19 +0200

    Created alerting service script

---
+++ app/scripts/services/alerting.js
@@ -0,0 +1,25 @@
+'use strict';
+/* global CryptoJS: false */
+
+angular.module('openhimWebui2App')
+  .factory('alerting', function (rootScope) {
+
+    var userProfile = {};
+
+    return {
+      alertConfirmDelete: function () {
+
+      },
+      alertServerError: function () {
+        
+      },
+      alertAddMsg: function () {
+        
+      },
+      alertRemoveMsg: function () {
+        
+      }
+
+    };
+
+  });
