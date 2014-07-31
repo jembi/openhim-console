dba6215de53a7589b42ff8a8d169db275a10480a
Martin Brocker <martinbrocker@yahoo.com>
2014-07-31 22:01:15 +0200

    Channeg Alerts UI additions

--- app/scripts/controllers/channelsmodal.js
+++ app/scripts/controllers/channelsmodal.js
@@ -3,9 +3,15 @@
 angular.module('openhimWebui2App')
   .controller('ChannelsModalCtrl', function ($scope, $modalInstance, Api, Notify, Alerting, channel) {
     
+
+    // get the users for the Channel Alert User dropdown
+    $scope.alertUsers = Api.Users.query();
+
+
     // get/set the users scope whether new or update
-    // backup object for the route being editted
+    // backup object for the route/alert being editted
     $scope.channelRoutesBackup = null;
+    $scope.channelAlertsBackup = null;
     $scope.routeWarnings = [];
     $scope.contentMatching = 'No matching';
     if (channel) {
@@ -20,6 +26,7 @@ angular.module('openhimWebui2App')
       $scope.channel = new Api.Channels();
     }
     $scope.newRoute = {};
+    $scope.newAlert = {};
 
 
 
@@ -69,7 +76,7 @@ angular.module('openhimWebui2App')
       if ($scope.update) {
         channel.$update(success, error);
       } else {
-        channel.$save({ channelName: '' }, success, error);
+        channel.$save({ channelId: '' }, success, error);
       }
     };
     /* ------------------------- Save/update channel record ---------------------------- */
@@ -78,7 +85,7 @@ angular.module('openhimWebui2App')
       $modalInstance.dismiss('cancel');
     };
 
-    /* -------------------------Add/edit/remove channel function---------------------------- */
+    /* -------------------------Add/edit/remove channel route function---------------------------- */
     $scope.addRoute = function (newRoute) {
       if (!$scope.channel.routes) {
         $scope.channel.routes = [];
@@ -113,7 +120,6 @@ angular.module('openhimWebui2App')
       // override backup route object to new route being editted
       $scope.channelRoutesBackup = angular.copy(route);
 
-      
       $scope.newRoute = route;
 
       // Check if any route warnings exist and add them to routeWarnings object
@@ -126,7 +132,58 @@ angular.module('openhimWebui2App')
       // Check if any route warnings exist and add them to routeWarnings object
       $scope.checkRouteWarnings();
     };
-    /* -------------------------Add/edit/remove channel function---------------------------- */
+    /* -------------------------Add/edit/remove channel route function---------------------------- */
+
+
+
+
+
+    /* -------------------------Add/edit/remove channel route function---------------------------- */
+    $scope.addAlert = function (newAlert) {
+      if (!$scope.channel.alerts) {
+        $scope.channel.alerts = [];
+      }
+      $scope.channel.alerts.push(angular.copy(newAlert));
+      // reset the backup route object when a record is added
+      $scope.channelAlertsBackup = null;
+
+      // Check if any route warnings exist and add them to routeWarnings object
+      //$scope.checkRouteWarnings();
+
+      // reset the backing object
+      $scope.newAlert.status = null;
+      $scope.newAlert.failureRate = null;
+      $scope.newAlert.groups = null;
+      $scope.newAlert.users = null;
+    };
+
+    $scope.editAlert = function (alertIndex, alert) {
+
+      // remove the selected alert object from scope
+      $scope.channel.alerts.splice(alertIndex, 1);
+
+      // if backup object exist update alerts object with backup alert
+      if ( $scope.channelAlertsBackup !== null ){
+        $scope.channel.alerts.push(angular.copy($scope.channelAlertsBackup));
+      }
+      // override backup alert object to new alert being editted
+      $scope.channelAlertsBackup = angular.copy(alert);
+
+      $scope.newAlert = alert;
+
+      // Check if any route warnings exist and add them to routeWarnings object
+      $scope.checkRouteWarnings();
+    };
+
+    $scope.removeAlert = function (alertIndex) {
+      $scope.channel.alerts.splice(alertIndex, 1);
+
+      // Check if any route warnings exist and add them to routeWarnings object
+      $scope.checkRouteWarnings();
+    };
+    /* -------------------------Add/edit/remove channel route function---------------------------- */
+
+
     
 
 
@@ -202,7 +259,108 @@ angular.module('openhimWebui2App')
       }
       return false;
     };
+
+
+
+    $scope.isAlertValid = function(){
+
+      if (!$scope.newAlert.status || !$scope.newAlert.users){
+        return false;
+      }
+      if ($scope.newAlert.status.length !== 3){
+        return false;
+      }
+      return true;
+
+    };
+
     /* ------------------ Validate form function --------------------- */
+
+
+
+
+
+
+
+
+
+    /***************************************************************/
+    /**   These are the functions for the Channel Alert Users     **/
+    /***************************************************************/
+
+    // define the alerts users backup object
+    $scope.channelAlertsUsersBackup = null;
+    $scope.newAlertUser = {};
+
+    $scope.addAlertUser = function (newAlertUser) {
+      if (!$scope.newAlert.users) {
+        $scope.newAlert.users = [];
+      }
+      $scope.newAlert.users.push(angular.copy(newAlertUser));
+      // reset the backup route object when a record is added
+      $scope.channelAlertsUsersBackup = null;
+
+      // reset the backing object
+      $scope.newAlertUser.user = '';
+      $scope.newAlertUser.method = '';
+      $scope.newAlertUser.maxAlerts = '';
+    };
+
+    $scope.editAlertUser = function (alertUserIndex, alertUser) {
+
+      // remove the selected alert user object from scope
+      $scope.newAlert.users.splice(alertUserIndex, 1);
+
+      // if backup object exist update alerts users object with backup alert user
+      if ( $scope.channelAlertsUsersBackup !== null ){
+        $scope.newAlert.users.push(angular.copy($scope.channelAlertsUsersBackup));
+      }
+      // override backup alert user object to new alert user being editted
+      $scope.channelAlertsUsersBackup = angular.copy(alertUser);
+
+      $scope.newAlertUser = alertUser;
+
+      // Check if any route warnings exist and add them to routeWarnings object
+      //$scope.checkRouteWarnings();
+    };
+
+    $scope.removeAlertUser = function (alertUserIndex) {
+      $scope.newAlert.users.splice(alertUserIndex, 1);
+    };
+
+    $scope.isAlertUserValid = function(){
+      if (!$scope.newAlertUser.user || !$scope.newAlertUser.method || !$scope.newAlertUser.maxAlerts){
+        return false;
+      }
+      return true;
+    };
+
+    /***************************************************************/
+    /**   These are the functions for the Channel Alert Users     **/
+    /***************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   });
