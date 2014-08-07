59b6514b3ebcc88cba547ecec190e00ea6e156fe
Martin Brocker <martinbrocker@yahoo.com>
2014-08-07 12:42:00 +0200

    Fixed JSHint errors in test scripts

--- test/spec/controllers/taskDetails.js
+++ test/spec/controllers/taskDetails.js
@@ -1,6 +1,5 @@
 'use strict';
 /* jshint expr: true */
-/* global sinon: false */
 
 describe('Controller: TaskDetailsCtrl', function () {
 
@@ -10,11 +9,11 @@ describe('Controller: TaskDetailsCtrl', function () {
   var scope, createController, httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $httpBackend, $modal) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
 
     httpBackend = $httpBackend;
 
-    $httpBackend.when('GET', new RegExp('.*/tasks/53e1eac5e907b57711509853')).respond({ "_id": "53e1eac5e907b57711509853", "completedDate": "2014-08-06", "remainingTransactions": 0, "user": "testuser", "created": "2014-08-06", "transactions": [{ "tid": "53e072e1ccbb302937ffb773", "tstatus": "Completed" }, { "tid": "53e064d1ccbb302937ffb772", "tstatus": "Completed" }], "status": "Completed" });
+    $httpBackend.when('GET', new RegExp('.*/tasks/53e1eac5e907b57711509853')).respond({ '_id': '53e1eac5e907b57711509853', 'completedDate': '2014-08-06', 'remainingTransactions': 0, 'user': 'testuser', 'created': '2014-08-06', 'transactions': [{ 'tid': '53e072e1ccbb302937ffb773', 'tstatus': 'Completed' }, { 'tid': '53e064d1ccbb302937ffb772', 'tstatus': 'Completed' }], 'status': 'Completed' });
 
     createController = function() {
       scope = $rootScope.$new();
--- test/spec/controllers/tasks.js
+++ test/spec/controllers/tasks.js
@@ -1,6 +1,5 @@
 'use strict';
 /* jshint expr: true */
-/* global sinon: false */
 
 describe('Controller: TasksCtrl', function () {
 
@@ -10,13 +9,13 @@ describe('Controller: TasksCtrl', function () {
   var scope, createController, httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $httpBackend, $modal) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
 
     httpBackend = $httpBackend;
 
     $httpBackend.when('GET', new RegExp('.*/tasks')).respond([
-      { "_id": "53e1eac5e907b57711509853", "completedDate": "2014-08-06", "remainingTransactions": 0, "user": "testuser", "created": "2014-08-06", "transactions": [{ "tid": "53e072e1ccbb302937ffb773", "tstatus": "Completed" }, { "tid": "53e064d1ccbb302937ffb772", "tstatus": "Completed" }], "status": "Completed" },
-      { "_id": "52e1eac5e807b57711509854", "completedDate": "2014-08-07", "remainingTransactions": 2, "user": "testuser", "created": "2014-08-07", "transactions": [{ "tid": "54e072e1ccbb302937ffb772", "tstatus": "Processing" }], "status": "Processing" }
+      { '_id': '53e1eac5e907b57711509853', 'completedDate': '2014-08-06', 'remainingTransactions': 0, 'user': 'testuser', 'created': '2014-08-06', 'transactions': [{ 'tid': '53e072e1ccbb302937ffb773', 'tstatus': 'Completed' }, { 'tid': '53e064d1ccbb302937ffb772', 'tstatus': 'Completed' }], 'status': 'Completed' },
+      { '_id': '52e1eac5e807b57711509854', 'completedDate': '2014-08-07', 'remainingTransactions': 2, 'user': 'testuser', 'created': '2014-08-07', 'transactions': [{ 'tid': '54e072e1ccbb302937ffb772', 'tstatus': 'Processing' }], 'status': 'Processing' }
     ]);
 
     $httpBackend.when('GET', new RegExp('.*/users')).respond([
