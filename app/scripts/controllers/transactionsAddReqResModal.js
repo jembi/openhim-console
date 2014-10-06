aa1eec36327ca52a799a9775218efecb318c28bd
Martin Brocker <martinbrocker@yahoo.com>
2014-10-06 14:29:25 +0200

    Updated transaction popup modal to show request body message and timestamp

--- app/scripts/controllers/transactionsAddReqResModal.js
+++ app/scripts/controllers/transactionsAddReqResModal.js
@@ -3,15 +3,23 @@
 angular.module('openhimWebui2App')
   .controller('TransactionsAddReqResModalCtrl', function ($scope, $modalInstance, record) {
 
-    $scope.viewFullBody = false;
     $scope.record = record;
+    $scope.viewFullBody = false;
+    $scope.viewFullBodyType = null;
+    $scope.viewFullBodyContent = null;
+    
 
-    $scope.toggleFullView = function () {
-      if ($scope.viewFullBody === true){
-        $scope.viewFullBody = false;
-      }else{
+    $scope.toggleFullView = function (type, bodyContent) {
+
+      // if both parameters supplied - view body message
+      if ( type && bodyContent ){
         $scope.viewFullBody = true;
+        $scope.viewFullBodyType = type;
+        $scope.viewFullBodyContent = bodyContent;
+      }else{
+        $scope.viewFullBody = false;
       }
+
     };
 
     $scope.cancel = function () {
