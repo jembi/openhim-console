4a2070aa60aa1e93a6f938b47289ce3e960c217c
Martin Brocker <martinbrocker@yahoo.com>
2014-10-08 17:10:07 +0200

    Console resource connects to HTTPS server

--- app/scripts/services/rest.js
+++ app/scripts/services/rest.js
@@ -4,33 +4,33 @@ angular.module('openhimWebui2App')
   .factory('Api', function ($resource, HOST, PORT) {
 
     return {
-      Authenticate: $resource('http://' + HOST + ':' + PORT + '/authenticate/:email'),
+      Authenticate: $resource('https://' + HOST + ':' + PORT + '/authenticate/:email'),
 
-      Channels: $resource('http://' + HOST + ':' + PORT + '/channels/:channelId', { channelId: '@_id' }, {
+      Channels: $resource('https://' + HOST + ':' + PORT + '/channels/:channelId', { channelId: '@_id' }, {
         update: { method: 'PUT' }
       }),
 
-      Users: $resource('http://' + HOST + ':' + PORT + '/users/:email', { email: '@email' }, {
+      Users: $resource('https://' + HOST + ':' + PORT + '/users/:email', { email: '@email' }, {
         update: { method: 'PUT' }
       }),
 
-      Clients: $resource('http://' + HOST + ':' + PORT + '/clients/:clientId', { clientId: '@_id' }, {
+      Clients: $resource('https://' + HOST + ':' + PORT + '/clients/:clientId', { clientId: '@_id' }, {
         update: { method: 'PUT' }
       }),
 
-      Transactions: $resource('http://' + HOST + ':' + PORT + '/transactions/:transactionId', { transactionId: '@_id' }),
+      Transactions: $resource('https://' + HOST + ':' + PORT + '/transactions/:transactionId', { transactionId: '@_id' }),
 
-      Mediators: $resource('http://' + HOST + ':' + PORT + '/mediators/:uuid', { uuid: '@uuid' }),
+      Mediators: $resource('https://' + HOST + ':' + PORT + '/mediators/:uuid', { uuid: '@uuid' }),
 
       // add the metric endpoints
-      Metrics: $resource('http://' + HOST + ':' + PORT + '/metrics/:type/:channelId', {}),
+      Metrics: $resource('https://' + HOST + ':' + PORT + '/metrics/:type/:channelId', {}),
       MetricsStatus: $resource('http://' + HOST + ':' + PORT + '/metrics/status', {}),
 
-      Tasks: $resource('http://' + HOST + ':' + PORT + '/tasks/:taskId', { taskId: '@_id' }, {
+      Tasks: $resource('https://' + HOST + ':' + PORT + '/tasks/:taskId', { taskId: '@_id' }, {
         update: { method: 'PUT' }
       }),
 
-      ContactGroups: $resource('http://' + HOST + ':' + PORT + '/groups/:groupId', { groupId: '@_id' }, {
+      ContactGroups: $resource('https://' + HOST + ':' + PORT + '/groups/:groupId', { groupId: '@_id' }, {
         update: { method: 'PUT' }
       })
 
