64992a5442e7115354e6fcc68b779030bb2cd2db
Duma Mtungwa <dumakude@gmail.com>
2015-03-27 15:06:59 +0200

    changed form fields

--- app/scripts/controllers/certificatesmodal.js
+++ app/scripts/controllers/certificatesmodal.js
@@ -2,6 +2,25 @@
 angular.module('openhimConsoleApp')
   .controller('CertificatesModalCtrl', function ($rootScope, $scope, $modalInstance, $timeout, Api, Notify, Alerting) {
 
+    var success = function () {
+      // add the success message
+      Alerting.AlertAddMsg('top', 'success', 'The client has been saved successfully');
+      notifyUser();
+
+    };
+
+    var error = function (err) {
+      // add the success message
+      Alerting.AlertAddMsg('top', 'danger', 'An error has occurred while saving the clients\' details: #' + err.status + ' - ' + err.data);
+      notifyUser();
+    };
+
+    var notifyUser = function(){
+      // reset backing object and refresh clients list
+      Notify.notify('clientsChanged');
+      $modalInstance.close();
+    };
+
     $scope.cancel = function () {
       $modalInstance.dismiss('cancel');
     };
@@ -16,24 +35,35 @@ angular.module('openhimConsoleApp')
       $scope.ngError = {};
       $scope.ngError.hasErrors = false;
 
-      // clientID validation
-      if( !$scope.cert.name ){
-        $scope.ngError.name = true;
+      // certificate validity validation
+      if( !$scope.cert.days){
+        $scope.ngError.days = true;
         $scope.ngError.hasErrors = true;
       }
     };
 
     $scope.submitFormCertificate = function () {
       $scope.validateFormCertificates();
+      console.log($scope.cert);
       // save the client object if no errors are present
       if ( $scope.ngError.hasErrors === false ){
         $scope.save($scope.cert);
       }
     };
 
-    $scope.certificateObject = new Api.Certificates();
+    $scope.cert = new Api.Certificates();
 
     $scope.save = function (cert) {
-      $scope.certificateObject = cert;
+      saveCert(cert);
+    };
+
+    var saveCert = function (cert) {
+      // set backup client object to check if cert has changed
+      $scope.certBackup = angular.copy(cert);
+      if ($scope.update) {
+        cert.$update(success, error);
+      } else {
+        cert.$save({ clientId: '' }, success, error);
+      }
     };
   });
