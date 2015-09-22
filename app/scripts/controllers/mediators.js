2c4148a1fe7be69a6425e098b25ac49398b93338
devcritter <venterh@gmail.com>
2015-09-22 10:52:07 +0200

    Issue #319 OHM-192: Added heartbeat to mediators page

--- app/scripts/controllers/mediators.js
+++ app/scripts/controllers/mediators.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global moment: false */
 
 angular.module('openhimConsoleApp')
   .controller('MediatorsCtrl', function ($scope, $modal, $location, Api, Alerting) {
@@ -12,6 +13,30 @@ angular.module('openhimConsoleApp')
       $scope.mediators = mediators;
       if( mediators.length === 0 ){
         Alerting.AlertAddMsg('bottom', 'warning', 'There are currently no mediators created');
+      } else {
+        angular.forEach(mediators, function(mediator){
+          var secondsDiffNow = function(mediator){
+            return Math.abs(Date.now()-moment(mediator._lastHeartbeat)) / 1000;
+          };
+
+          if (!mediator._lastHeartbeat) {
+            mediator.lastHeartbeatStatus = 'never';
+          } else {
+            if (secondsDiffNow(mediator) < 60) {
+              mediator.lastHeartbeatStatus = 'success';
+            } else if (secondsDiffNow(mediator) < 120) {
+              mediator.lastHeartbeatStatus = 'warning';
+            } else {
+              mediator.lastHeartbeatStatus = 'danger';
+            }
+            mediator.lastHeartbeatDisplay = moment(mediator._lastHeartbeat).fromNow();
+          }
+
+          if (mediator._uptime) {
+            //generate human-friendly display string, e.g. 4 days
+            mediator.uptimeDisplay = moment().subtract(mediator._uptime, 'seconds').fromNow(true);
+          }
+        });
       }
     };
 
