c4ae308e801ce5cc5e1ecc25542b1b98632ff3fe
devcritter <venterh@gmail.com>
2015-11-24 13:54:36 +0200

    Mediator config: Refactored controls using directives. Added handling for struct type.

---
+++ app/scripts/directives/mediator-config-display.js
@@ -0,0 +1,44 @@
+'use strict';
+// Mediator Config Display Directive
+//
+// Makes use of recursive rendering trick from here:
+// http://sporto.github.io/blog/2013/06/24/nested-recursive-directives-in-angular/
+
+angular.module('openhimConsoleApp')
+  .directive('mediatorConfigDisplay', function(){
+    return{
+      restrict: 'EA',
+      scope: {
+        configDefs: '=',
+        config: '='
+      },
+      templateUrl: 'views/partials/mediator-config-display.html',
+      link: function(scope){
+        scope.mediatorDefsMap = {}
+
+        scope.$watch('configDefs', function(configDefs){
+          if (configDefs) {
+            configDefs.map(function(def){
+              scope.mediatorDefsMap[def.param] = def;
+            });
+          }
+        });
+      }
+    };
+  })
+  .directive('mediatorNestedConfigDisplay', function($compile){
+    return{
+      restrict: 'EA',
+      scope: {
+        configDefs: '=',
+        config: '='
+      },
+      template: '<div/>',
+      link: function(scope, element){
+        if (scope.config) {
+          element.append('<div mediator-config-display config-defs="configDefs" config="config"/>');
+          $compile(element.contents())(scope);
+        }
+      }
+    };
+  });
---
+++ app/scripts/directives/mediator-config.js
@@ -0,0 +1,62 @@
+'use strict';
+// Mediator Config Directive
+//
+// Makes use of recursive rendering trick from here:
+// http://sporto.github.io/blog/2013/06/24/nested-recursive-directives-in-angular/
+
+angular.module('openhimConsoleApp')
+  .directive('mediatorConfig', function(){
+    return{
+      restrict: 'EA',
+      scope: {
+        configDefs: '=',
+        config: '='
+      },
+      templateUrl: 'views/partials/mediator-config.html',
+      link: function(scope){
+        scope.inputKeys = {};
+        scope.inputValues = {};
+
+        scope.removeMapping = function (param, mapping) {
+          delete scope.config[param][mapping];
+        };
+
+        scope.addNewMapping = function (param) {
+          if (!scope.config[param]) {
+            scope.config[param] = {};
+          }
+          scope.config[param][scope.inputKeys[param]] = scope.inputValues[param];
+          scope.inputKeys[param] = '';
+          scope.inputValues[param] = '';
+        };
+
+        scope.doesNewKeyExist = function (param) {
+          return scope.config[param] && scope.config[param][scope.inputKeys[param]];
+        };
+
+        scope.isNewKeyValid = function (param) {
+          return scope.inputKeys[param] && !scope.doesNewKeyExist(param);
+        };
+      }
+    };
+  })
+  .directive('mediatorNestedConfig', function($compile){
+    return{
+      restrict: 'EA',
+      scope: {
+        configDefs: '=',
+        config: '='
+      },
+      template: '<div/>',
+      link: function(scope, element){
+        if (scope.configDefs) {
+          if (!scope.config) {
+            scope.config = {}
+          }
+
+          element.append('<div mediator-config config-defs="configDefs" config="config"/>');
+          $compile(element.contents())(scope);
+        }
+      }
+    };
+  });
