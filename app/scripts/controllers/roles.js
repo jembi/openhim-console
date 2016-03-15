e8a7686b014b352e8cd82c99f8459e2194d1f458
napiergit <andrew.napier@jembi.org>
2016-03-15 10:02:38 +0200

    Remove jump by creating a front end object

--- app/scripts/controllers/roles.js
+++ app/scripts/controllers/roles.js
@@ -3,12 +3,16 @@
 angular.module('openhimConsoleApp')
   .controller('RolesCtrl', function ($rootScope, $scope, $modal, $interval, Api, Notify, Alerting) {
     
-    $scope.clientsObject = {};
-    $scope.channelsObject = {};
-    $scope.rolesObject = {};
+    
+    var clientsMirror = null;
+    var channelsMirror = null;
+    var rolesMirror = null;
     
     /* -------------------------Load Clients---------------------------- */
     var clientQuerySuccess = function(clients){
+      if(!clientsMirror) {
+        $scope.clientsMirror = clients;
+      }
       $scope.clients = clients;
       if( clients.length === 0 ){
         Alerting.AlertAddMsg('bottom', 'warning', 'There are currently no clients created');
@@ -61,6 +65,9 @@ angular.module('openhimConsoleApp')
     
     var loadRoles = function () {
       var roleQuerySuccess = function(roles){
+        if(!rolesMirror) {
+          $scope.rolesMirror = roles;
+        }
         $scope.roles = roles;
         if( roles.length === 0 ){
           Alerting.AlertAddMsg('bottom', 'warning', 'There are currently no roles created');
@@ -89,39 +96,61 @@ angular.module('openhimConsoleApp')
       console.log('hello');
     }
     
-    
-     
     /* -------------------------Assign Clients to Roles---------------------------- */
-    $scope.roleAssignedToClient = function(client, role) {
-      if(client.roles) {
-        for (var i=0;i<client.roles.length;i++) {
-          if (client.roles[i] == role.name) {
-            return true;
-          }
-        }
-        return false;
+    $scope.clientRoles = {};
+
+    $scope.$watch('clientsMirror', function (newVal, oldVal) {
+      if(newVal) {
+        waitForRolesMirror();
       }
-    };
+    });
     
+    var waitForRolesMirror = function() {
+      $scope.$watch('rolesMirror', function (newVal, oldVal) {
+        if(newVal) {
+          buildClientRolesObject();
+        }
+      });
+    }
+    
+    var buildClientRolesObject = function() {
+      angular.forEach($scope.clientsMirror, function(client) { 
+        angular.forEach($scope.rolesMirror, function(role) {
+          $scope.clientRoles[client.name + role.name] = false;
+          for (var i=0;i<client.roles.length;i++) {
+            if (client.roles[i] == role.name) {
+              $scope.clientRoles[client.name + role.name] = true;
+            }
+          }
+        });
+      });
+    }
+
     $scope.assignRoleToClient = function(client, role) {
+      $scope.clientRoles[client.name + role.name] = true;
       client.roles.push(role.name);
-      client.$update(function(result) {
+      Api.Clients.update({}, client , function(response){
         Notify.notify('clientsChanged');
-      }, function(){});
+      }, function(error) {
+        Alerting.AlertAddMsg('server', 'error', error);
+      });
     };
     
     $scope.removeRoleFromClient = function(client, role) {
+      $scope.clientRoles[client.name + role.name] = false;
       var index = client.roles.indexOf(role.name);
       client.roles.splice(index, 1);
-      client.$update(function(result) {
+      Api.Clients.update({}, client , function(response) {
         Notify.notify('clientsChanged');
-      }, function(){});
+      }, function(error) {
+        Alerting.AlertAddMsg('server', 'error', error);
+      });
     };
     
     $scope.toggleEditClients = function() {
       $scope.editClients = $scope.editClients === true ? false : true;
     };
-    /* -------------------------Assign Clients to Roles---------------------------- */
+    /* -------------------------End Assign Clients to Roles---------------------------- */
     
     
     /* -------------------------Assign Roles To Channels---------------------------- */
@@ -134,26 +163,27 @@ angular.module('openhimConsoleApp')
       return false;
     };
     
-    $scope.assignChannelToRole = function(channel, role) {
-      role.channels.push({"_id":channel._id, "name":channel.name});
-      role.$update(function(result) {
-        Notify.notify('rolesChanged');
-      }, function(){});
-    };
-    
-    $scope.removeChannelFromRole = function(channel, role) {
-      var index = -1;
-      for(var i = 0; i < role.channels.length; i++) {
-        if (role.channels[i]._id === channel._id) {
-            index = i;
-            break;
-        }
-      }
-      role.channels.splice(index, 1);
-      role.$update(function(result) {
-        Notify.notify('rolesChanged');
-      }, function(){});
-    };
+    // This code will come in handy when we create a roles collection
+    // $scope.assignChannelToRole = function(channel, role) {
+    //   role.channels.push({"_id":channel._id, "name":channel.name});
+    //     Api.Channels.update({}, role, function(result) {
+    //     Notify.notify('rolesChanged');
+    //   }, function(){});
+    // };
+    // 
+    // $scope.removeChannelFromRole = function(channel, role) {
+    //   var index = -1;
+    //   for(var i = 0; i < role.channels.length; i++) {
+    //     if (role.channels[i]._id === channel._id) {
+    //         index = i;
+    //         break;
+    //     }
+    //   }
+    //   role.channels.splice(index, 1);
+    //   Api.Channels.update({}, role, function(result) {
+    //     Notify.notify('rolesChanged');
+    //   }, function(){});
+    // };
     
     $scope.assignRoleToChannel = function(channel, role) {
       channel.allow.push(role.name);
