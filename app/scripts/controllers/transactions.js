9ae1412daf4d39302e8038596b976d05f002c462
napiergit <andrew.napier@jembi.org>
2016-09-13 16:36:09 +0200

    OHM-355: Add client name to transaction list

--- app/scripts/controllers/transactions.js
+++ app/scripts/controllers/transactions.js
@@ -221,9 +221,19 @@ angular.module('openhimConsoleApp')
         }
       });
     }, function(){ /* server error - could not connect to API to get channels */ });
-
-    // clients used for advanced filter dropdown
-    $scope.clients = Api.Clients.query();
+    
+    
+    var clientsIdNameMap = [];
+    
+    var clientsSuccess = function(clients) {
+      $scope.clients = clients;
+      // Create client map of id to name
+      $scope.clients.map(function(client) {
+        clientsIdNameMap[client._id] = client.name;
+      });
+    }
+    // clients used for advanced filter dropdown and client name on transaction list
+    Api.Clients.query(clientsSuccess);
 
     /***************************************************/
     /**         Initial page load functions           **/
@@ -748,6 +758,11 @@ angular.module('openhimConsoleApp')
         // update user via api
         updateUser(currentUser);
       });
+    };
+    
+    // Get client name from client id
+    $scope.getClientNameFromClientID = function(clientId) {
+      return clientsIdNameMap[clientId];
     };
 
     /*******************************************************************/
