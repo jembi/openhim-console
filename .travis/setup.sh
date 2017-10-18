4824737db476a3fc5eed7b2fa1866829f8a6901d
Nathan Floor <nathan.floor@jembi.org>
2017-10-18 05:41:09 +0000

    OHM-511: updated travis encryption variables in setup file

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_78888cd548b2_key -iv $encrypted_78888cd548b2_iv -in .travis/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_454006dcd4e6_key -iv $encrypted_454006dcd4e6_iv -in .travis/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 mv deploy_key ~/.ssh/id_rsa
 
