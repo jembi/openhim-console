c8cdedeb7b934d3454c3ae110a2aab67fdc36f63
Nathan Floor <nathan.floor@jembi.org>
2017-11-07 08:47:14 +0200

    removed uneeded scripts and updated travis config

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ([ "$TRAVIS_BRANCH" == "master" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "test" ] || [ "$TRAVIS_BRANCH" == "staging" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     curl -H 'Content-Type: application/json' --data '{"source_type": "Branch", "source_name": "console"}' -X POST https://registry.hub.docker.com/u/jembi/openhim-console/trigger/f4e50fb6-7d96-4668-84bb-b456ba4fab6e/
 else
-    echo "Docker image will only be built for commits to master"
+    echo "Docker image will only be built for commits to master/test/staging"
 fi
