f87b447c8f296cb4d20c71e1d54fd494cce949c1
Nathan Floor <nathan.floor@jembi.org>
2017-11-16 12:14:23 +0200

    fixed typo in build_script

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
     curl -H 'Content-Type: application/json' --data '{"source_type": "Branch", "source_name": "console"}' -X POST https://registry.hub.docker.com/u/jembi/openhim-console/trigger/f4e50fb6-7d96-4668-84bb-b456ba4fab6e/
 elif [ "$TRAVIS_BRANCH" == "test" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     curl -H 'Content-Type: application/json' --data '{"docker_tag": "test"}' -X POST https://registry.hub.docker.com/u/jembi/openhim-console/trigger/f4e50fb6-7d96-4668-84bb-b456ba4fab6e/
-elif f [ "$TRAVIS_BRANCH" == "staging" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+elif [ "$TRAVIS_BRANCH" == "staging" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     curl -H 'Content-Type: application/json' --data '{"docker_tag": "staging"}' -X POST https://registry.hub.docker.com/u/jembi/openhim-console/trigger/f4e50fb6-7d96-4668-84bb-b456ba4fab6e/
 else
     echo "Docker image will only be built for commits to master/test/staging"
