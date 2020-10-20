efa3be0255cef14c3494c49ad33a69550e71bc9c
brad <bradford.sawadye@jembi.org>
2020-10-20 16:35:45 +0200

    add logic for retrieving orchestration req/res bodies
    OHM-1038

--- app/scripts/controllers/transactionsAddReqResModal.js
+++ app/scripts/controllers/transactionsAddReqResModal.js
@@ -1,9 +1,10 @@
 import {
   beautifyIndent,
-  returnContentType
+  returnContentType,
+  retrieveBodyProperties
 } from '../utils'
 
-export function TransactionsAddReqResModalCtrl ($scope, $uibModal, $uibModalInstance, record, channel, transactionId, recordType, index) {
+export function TransactionsAddReqResModalCtrl ($scope, $uibModal, $uibModalInstance, Api, config, record, channel, transactionId, recordType, index) {
   $scope.record = record
   $scope.channel = channel // optional
   $scope.viewFullBody = false
@@ -14,22 +15,60 @@ export function TransactionsAddReqResModalCtrl ($scope, $uibModal, $uibModalInst
   $scope.recordPathRequest = recordType + '[' + index + '].request'
   $scope.recordPathResponse = recordType + '[' + index + '].response'
 
+  const defaultLengthOfBodyToDisplay = config.defaultLengthOfBodyToDisplay
+  $scope.partialRecordResponseBody = false
+  $scope.partialRecordRequestBody = false
+
   // transform request body with indentation/formatting
-  if (record.request && record.request.body) {
-    if (record.request.headers && returnContentType(record.request.headers)) {
-      let requestTransform = beautifyIndent(returnContentType(record.request.headers), record.request.body)
-      $scope.record.request.body = requestTransform.content
-      $scope.requestTransformLang = requestTransform.lang
+  if (record.request && record.request.bodyId) {
+    $scope.retrieveRecordRequestBody = function (start=0, end=defaultLengthOfBodyToDisplay) {
+      Api.TransactionBodies(transactionId, record.request.bodyId, start, end).then(response => {
+        const { start, end, bodyLength } = retrieveBodyProperties(response)
+
+        if (start && end && bodyLength) {
+          $scope.recordRequestBodyStart = start
+          $scope.recordRequestBodyEnd = end
+          $scope.recordRequestBodyLength = bodyLength
+
+          if ((bodyLength - end) > 1) {
+            $scope.partialRecordRequestBody = true
+          }
+        }
+
+        if (record.request.headers && returnContentType(record.request.headers)) {
+          let requestTransform = beautifyIndent(returnContentType(record.request.headers), response.data)
+          $scope.record.request.body = requestTransform.content
+          $scope.requestTransformLang = requestTransform.lang
+        }
+      })
     }
+    $scope.retrieveRecordRequestBody()
   }
 
   // transform response body with indentation/formatting
-  if (record.response && record.response.body) {
-    if (record.response.headers && returnContentType(record.response.headers)) {
-      let responseTransform = beautifyIndent(returnContentType(record.response.headers), record.response.body)
-      $scope.record.response.body = responseTransform.content
-      $scope.responseTransformLang = responseTransform.lang
+  if (record.response && record.response.bodyId) {
+    $scope.retrieveRecordResponseBody = function (start=0, end=defaultLengthOfBodyToDisplay) {
+      Api.TransactionBodies(transactionId, record.response.bodyId, start, end).then(response => {
+        const { start, end, bodyLength } = retrieveBodyProperties(response)
+
+        if (start && end && bodyLength) {
+          $scope.recordResponseBodyStart = start
+          $scope.recordResponseBodyEnd = end
+          $scope.recordResponseBodyLength = bodyLength
+
+          if ((bodyLength - end) > 1) {
+            $scope.partialRecordResponseBody = true
+          }
+        }
+
+        if (record.response.headers && returnContentType(record.response.headers)) {
+          let responseTransform = beautifyIndent(returnContentType(record.response.headers), response.data)
+          $scope.record.response.body = responseTransform.content
+          $scope.responseTransformLang = responseTransform.lang
+        }
+      })
     }
+    $scope.retrieveRecordResponseBody()
   }
 
   $scope.toggleFullView = function (type, bodyContent, contentType) {
