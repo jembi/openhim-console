409aaed54e415976c526e7e5a61789ae10a5e40f
brad <bradford.sawadye@jembi.org>
2020-10-21 12:39:58 +0200

    add logic for retrieving the full body for downloading
    OHM-1038

--- app/scripts/directives/transaction-body-downloader.js
+++ app/scripts/directives/transaction-body-downloader.js
@@ -2,7 +2,7 @@ import * as _ from 'lodash'
 import { saveAs } from 'file-saver'
 import { buildBlob } from '../utils'
 
-export function transactionBodyDownloader (Api) {
+export function transactionBodyDownloader (Api, Alerting) {
   return {
     restrict: 'EA',
     template: '<div class="btn btn-primary" ng-click="download()" tooltip="Download body"><i class="glyphicon glyphicon-download-alt"></i></div>',
@@ -29,13 +29,19 @@ export function transactionBodyDownloader (Api) {
             extension = '.txt'
           }
 
-          let bodyBlob = buildBlob(subTrx.body, contentType)
-          let filename = scope.transactionId + '_' + _.camelCase(scope.path) + extension
-          saveAs(bodyBlob, filename)
+          if(subTrx.bodyId) {
+            Api.TransactionBodies(trx._id, subTrx.bodyId).then(response => {
+              let bodyBlob = buildBlob(response.data, contentType)
+              let filename = scope.transactionId + '_' + _.camelCase(scope.path) + extension
+              saveAs(bodyBlob, filename)
+            }).catch(onError)
+          } else {
+            Alerting.AlertAddServerMsg('Invalid body Id')
+          }
         }
 
         let onError = function (err) {
-          console.log(err)
+          Alerting.AlertAddServerMsg(err.status)
         }
 
         Api.Transactions.get({ transactionId: scope.transactionId, filterRepresentation: 'full' }, onSuccess, onError)
