1ef246139fd33e8c7bd82893319c894b8f60001c
Ryan Crichton <ryan@jembi.org>
2020-10-26 09:51:21 +0200

    WIP: explore loading more of the body
    We still need to store state of how much of the body we have pulled and add logic for assembling the body request by request and we retireve more of it.
    
    OHM-1040

--- app/scripts/controllers/transactionsBodyModal.js
+++ app/scripts/controllers/transactionsBodyModal.js
@@ -1,11 +1,13 @@
 import { beautifyIndent, returnContentType, retrieveBodyProperties } from '../utils'
 import { Api } from '../services/rest'
 
-export function TransactionsBodyModalCtrl ($scope, $uibModalInstance, config, Alerting, bodyData) {
+export function TransactionsBodyModalCtrl ($scope, $uibModalInstance, config, Alerting, bodyData, Api) {
   $scope.bodyData = bodyData
   const defaultLengthOfBodyToDisplay = config.defaultLengthOfBodyToDisplay
 
-  $scope.partialBody = false
+  console.log(bodyData)
+
+  // $scope.partialBody = false
   // transform body with indentation/formatting
   if ($scope.bodyData && $scope.bodyData.content) {
     if (bodyData.headers && returnContentType(bodyData.headers)) {
@@ -14,37 +16,39 @@ export function TransactionsBodyModalCtrl ($scope, $uibModalInstance, config, Al
     }
   }
 
-  if (
-    $scope.bodyData &&
-    $scope.bodyData.transactionId &&
-    $scope.bodyData.bodyId
-  ) {
-    $scope.retrieveBodyData = function (start=0, end=defaultLengthOfBodyToDisplay) {
-      Api.TransactionBodies($scope.bodyData.transactionId, $scope.bodyData.bodyId, start, end).then(response => {
-        const { start, end, bodyLength } = retrieveBodyProperties(response)
+  $scope.retrieveBodyData = function (start=0, end=defaultLengthOfBodyToDisplay) {
+    Api.TransactionBodies($scope.bodyData.transactionId, $scope.bodyData.bodyId, start, end).then(response => {
+      const { start, end, bodyLength } = retrieveBodyProperties(response)
 
-        if (start && end && bodyLength) {
-          $scope.bodyStart = start
-          $scope.bodyEnd = end
-          $scope.bodyLength = bodyLength
+      if (start && end && bodyLength) {
+        $scope.bodyStart = start
+        $scope.bodyEnd = end
+        $scope.bodyLength = bodyLength
 
-          if ((bodyLength - end) > 1) {
-            $scope.partialBody = true
-          }
+        if ((bodyLength - end) > 1) {
+          $scope.partialBody = true
         }
+      }
 
-        if (bodyData.headers && returnContentType(bodyData.headers)) {
-          let bodyTransform = beautifyIndent(returnContentType(bodyData.headers), response.data)
-          $scope.bodyData.content = bodyTransform.content
-        }
-      }).catch(err => {
-        Alerting.AlertAddServerMsg(err.status)
-      })
-    }
-    $scope.retrieveBodyData()
+      if (bodyData.headers && returnContentType(bodyData.headers)) {
+        let bodyTransform = beautifyIndent(returnContentType(bodyData.headers), response.data)
+        $scope.bodyData.content = bodyTransform.content
+      }
+    }).catch(err => {
+      Alerting.AlertAddServerMsg(err.status)
+    })
   }
+  // $scope.retrieveBodyData()
 
   $scope.cancel = function () {
     $uibModalInstance.dismiss('cancel')
+  }
+
+  $scope.loadMore = function () {
+    $scope.retrieveBodyData(0, 2048)
+  }
+
+  $scope.loadFull = function () {
+    $scope.retrieveBodyData(0, 9999999999999)
   }
 }
