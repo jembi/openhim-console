24f6aa26d01d2e02799afbf855b325a016ae36eb
nour-borgi <nour-hooda-borgi@hotmail.com>
2023-02-03 11:13:55 +0100

    Fix the rest of the tests related to authentication

--- test/spec/controllers/profile.js
+++ test/spec/controllers/profile.js
@@ -13,15 +13,22 @@ describe('Controller: ProfileCtrl', function () {
   })
 
   var scope, createController, httpBackend
+  var meResponse = {
+    user: {
+      email: 'test@user.org',
+      firstname: 'test',
+      surname: 'test',
+      groups: [
+        'admin'
+      ]
+    }
+  }
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     httpBackend = $httpBackend
 
-    httpBackend.when('GET', new RegExp('.*/authenticate/test@user.org')).respond({
-      salt: 'test-salt',
-      ts: 'test-ts'
-    })
+    httpBackend.when('POST', new RegExp('.*/authenticate/local')).respond(201)
 
     httpBackend.when('GET', new RegExp('config/visualizer.json')).respond({
       components: [],
@@ -59,9 +66,6 @@ describe('Controller: ProfileCtrl', function () {
       _id: '539846c240f2eb682ffeca4b',
       email: 'test@user.org',
       firstname: 'test',
-      passwordAlgorithm: 'sha512',
-      passwordHash: '7d0d1a30d16f5343e3390fe9ef1dd61539a7f797267e0d2241ed22390dfc9743091244ddb2463df2f1adf6df3c355876ed34c6523f1e8d3b7f16f4b2afc8c160',
-      passwordSalt: 'test-salt',
       surname: 'test',
       weeklyAlert: true,
       dailyAlert: true,
@@ -73,8 +77,8 @@ describe('Controller: ProfileCtrl', function () {
     })
 
     $httpBackend.when('GET', new RegExp('.*/users')).respond([
-      { firstname: 'Super', surname: 'User', email: 'super@openim.org', passwordAlgorithm: 'sample/api', passwordHash: '539aa778930879b01b37ff62', passwordSalt: '79b01b37ff62', groups: ['admin'], settings: {} },
-      { firstname: 'Ordinary', surname: 'User', email: 'normal@openim.org', passwordAlgorithm: 'sample/api', passwordHash: '539aa778930879b01b37ff62', passwordSalt: '79b01b37ff62', groups: ['limited'], settings: {} }
+      { firstname: 'Super', surname: 'User', email: 'super@openim.org', groups: ['admin'], settings: {} },
+      { firstname: 'Ordinary', surname: 'User', email: 'normal@openim.org', groups: ['limited'], settings: {} }
     ])
 
     $httpBackend.when('GET', new RegExp('.*/channels')).respond([
@@ -85,6 +89,8 @@ describe('Controller: ProfileCtrl', function () {
     httpBackend.when('PUT', new RegExp('.*/users')).respond('user has been successfully updated')
 
     createController = function () {
+      $httpBackend.when('GET', new RegExp('.*/me')).respond(meResponse)
+
       scope = $rootScope.$new()
       scope.consoleSession = {}
       scope.consoleSession.sessionUser = 'test@user.org'
@@ -170,13 +176,10 @@ describe('Controller: ProfileCtrl', function () {
     scope.temp.password = 'password'
     scope.temp.passwordConfirm = 'password'
 
-    // Should submit the form with supplied values annd save the user with new password salt/hash
+    // Should submit the form with supplied values annd save the user with new password
     scope.submitFormProfile()
-    scope.user.$update.should.have.been.called()
     scope.ngError.should.have.property('hasErrors', false)
 
-    scope.user.should.have.property('passwordSalt')
-    scope.user.should.have.property('passwordHash')
     scope.user.should.have.property('firstname', 'Jane')
     scope.user.should.have.property('surname', 'Doe')
     scope.user.should.have.property('weeklyAlert', true)
