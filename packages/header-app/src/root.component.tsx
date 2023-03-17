7a9049a03a7d1b7ac187800521b713a61bea7ea4
Ryan Crichton <ryan@jembi.org>
2023-03-17 16:53:38 +0200

    Make sidebar and header responsive

--- packages/header-app/src/root.component.tsx
+++ packages/header-app/src/root.component.tsx
@@ -1,33 +1,33 @@
 import styled from '@emotion/styled'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { green } from '@mui/material/colors';
-import { AccountCircle } from '@mui/icons-material';
-import { IconButton, Link, Menu, MenuItem } from '@mui/material';
-import React from 'react';
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import {createTheme, ThemeProvider} from '@mui/material/styles'
+import {green} from '@mui/material/colors'
+import {AccountCircle, Menu as MenuIcon} from '@mui/icons-material'
+import {Drawer, IconButton, Link, Menu, MenuItem} from '@mui/material'
+import React from 'react'
+import {OpenHIMMenu} from '@jembi/openhim-sidebar'
 
 const theme = createTheme({
   typography: {
     // Tell MUI what's the font-size on the html element is.
     // This is necessary as the legacy angular app uses bootstrap which
-    // changes the html font-size golbaly to 10px
-    htmlFontSize: 10,
+    // changes the html font-size globally to 10px
+    htmlFontSize: 10
   },
   palette: {
     primary: {
-      main: green[700],
+      main: green[700]
     },
     secondary: {
-      main: green['A100'],
-    },
+      main: green['A100']
+    }
   }
-});
+})
 
 const GrowingDiv = styled.div`
-  flex-grow: 1
+  flex-grow: 1;
 `
 
 export default function ButtonAppBar() {
@@ -35,17 +35,46 @@ export default function ButtonAppBar() {
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
-  };
+  }
 
   const handleClose = () => {
     setAnchorEl(null)
-  };
+  }
+
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1 }}>
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true // Better open performance on mobile.
+        }}
+        sx={{
+          display: {xs: 'block', sm: 'none'},
+          '& .MuiDrawer-paper': {boxSizing: 'border-box', width: '200px'}
+        }}
+      >
+        <OpenHIMMenu></OpenHIMMenu>
+      </Drawer>
+      <Box sx={{flexGrow: 1}}>
         <AppBar position="static">
           <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{mr: 2, display: {sm: 'none'}}}
+            >
+              <MenuIcon />
+            </IconButton>
             <a href="#!/transactions">
               <img height={50} src="/images/openhim-logo-green.png"></img>
             </a>
@@ -66,23 +95,35 @@ export default function ButtonAppBar() {
                 anchorEl={anchorEl}
                 anchorOrigin={{
                   vertical: 'top',
-                  horizontal: 'right',
+                  horizontal: 'right'
                 }}
                 keepMounted
                 transformOrigin={{
                   vertical: 'top',
-                  horizontal: 'right',
+                  horizontal: 'right'
                 }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
-                <MenuItem onClick={handleClose} component={Link} href="#!/profile">Profile</MenuItem>
-                <MenuItem onClick={handleClose} component={Link} href="#!/logout">Logout</MenuItem>
+                <MenuItem
+                  onClick={handleClose}
+                  component={Link}
+                  href="#!/profile"
+                >
+                  Profile
+                </MenuItem>
+                <MenuItem
+                  onClick={handleClose}
+                  component={Link}
+                  href="#!/logout"
+                >
+                  Logout
+                </MenuItem>
               </Menu>
             </div>
           </Toolbar>
         </AppBar>
       </Box>
-    </ThemeProvider >
-  );
+    </ThemeProvider>
+  )
 }
--- packages/sidebar-app/src/root.component.tsx
+++ packages/sidebar-app/src/root.component.tsx
@@ -1,134 +1,33 @@
-import { ThemeProvider } from '@emotion/react';
-import Box from '@mui/material/Box';
-import { green, purple } from '@mui/material/colors';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { createTheme } from '@mui/material/styles';
-import { useState, useEffect } from 'react';
+import {ThemeProvider} from '@emotion/react'
+import {Box} from '@mui/material'
+import {green} from '@mui/material/colors'
+import {createTheme} from '@mui/material/styles'
+import {useState, useEffect} from 'react'
+import OpenHIMMenu from './menu.component'
 
 const theme = createTheme({
   typography: {
     // Tell MUI what's the font-size on the html element is.
     // This is necessary as the legacy angular app uses bootstrap which
-    // changes the html font-size golbaly to 10px
-    htmlFontSize: 10,
+    // changes the html font-size globally to 10px
+    htmlFontSize: 10
   },
   palette: {
     primary: {
-      main: green[700],
+      main: green[700]
     },
     secondary: {
-      main: green['A100'],
-    },
+      main: green['A100']
+    }
   }
-});
+})
 
 export default function Root(props) {
-  const [isAdmin, setIsAdmin] = useState(false)
-
-  useEffect(() => {
-    async function fetchMe() {
-      const resConf = await fetch('/config/default.json')
-      if (!resConf.ok) {
-        return console.error('[sidebar-app] Failed to fetch OpenHIM console config')
-      }
-      const { protocol, host, hostPath, port } = await resConf.json()
-      const resMe = await fetch(`${protocol}://${host}:${port}${(/^\s*$/.test(hostPath) ? '' : '/' + hostPath)}/me`, { credentials: 'include' })
-      if (!resMe.ok) {
-        return console.error('[sidebar-app] Failed to fetch user profile')
-      }
-      const me = await resMe.json()
-
-      setIsAdmin(me.user.groups.includes('admin'))
-    }
-
-    fetchMe()
-  })
-
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ width: '100%', maxWidth: 250, bgcolor: 'background.paper' }}>
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#!">
-              <ListItemText primary="Dashboard" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="/#!/transactions">
-              <ListItemText primary="Transaction Log" />
-            </ListItemButton>
-          </ListItem>
-          {isAdmin && (
-            <>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/audits">
-                  <ListItemText primary="Audit Log" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/clients">
-                  <ListItemText primary="Clients" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/channels">
-                  <ListItemText primary="Channels" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/tasks">
-                  <ListItemText primary="Tasks" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/visualizer">
-                  <ListItemText primary="Visualizer" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/groups">
-                  <ListItemText primary="Contact Lists" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/mediators">
-                  <ListItemText primary="Mediators" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/users">
-                  <ListItemText primary="Users" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/certificates">
-                  <ListItemText primary="Certificates" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/export-import">
-                  <ListItemText primary="Import/Export" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="/#!/logs">
-                  <ListItemText primary="Server Logs" />
-                </ListItemButton>
-              </ListItem>
-            </>
-          )}
-          <Divider />
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="/#!/about">
-              <ListItemText primary="About" />
-            </ListItemButton>
-          </ListItem>
-        </List>
+      <Box sx={{display: {xs: 'none', sm: 'block'}, 'min-width': '200px'}}>
+        <OpenHIMMenu></OpenHIMMenu>
       </Box>
     </ThemeProvider>
-  );
+  )
 }
