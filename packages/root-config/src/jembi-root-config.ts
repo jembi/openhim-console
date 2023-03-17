376cb5982ca54d4b1f9c929ed87b2096aafc40d1
Ryan Crichton <ryan@jembi.org>
2023-03-17 16:54:59 +0200

    Reformat all microfrontends to use OpenHIM prettier config

--- packages/root-config/src/jembi-root-config.ts
+++ packages/root-config/src/jembi-root-config.ts
@@ -1,20 +1,20 @@
-import { registerApplication, start } from "single-spa";
+import {registerApplication, start} from 'single-spa'
 import {
   constructApplications,
   constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
+  constructLayoutEngine
+} from 'single-spa-layout'
+import microfrontendLayout from './microfrontend-layout.html'
 
-const routes = constructRoutes(microfrontendLayout);
+const routes = constructRoutes(microfrontendLayout)
 const applications = constructApplications({
   routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
+  loadApp({name}) {
+    return System.import(name)
+  }
+})
+const layoutEngine = constructLayoutEngine({routes, applications})
 
-applications.forEach(registerApplication);
-layoutEngine.activate();
-start();
+applications.forEach(registerApplication)
+layoutEngine.activate()
+start()
