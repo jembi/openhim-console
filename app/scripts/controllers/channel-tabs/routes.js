57cf9a8bb29044e29a2a47b545fd5de7c59c0713
Arran Standish <arranstandish@hotmail.com>
2023-04-24 08:08:32 +0200

    Remove the hard coded http codes and instead check against the range of 100-599

--- app/scripts/controllers/channel-tabs/routes.js
+++ app/scripts/controllers/channel-tabs/routes.js
@@ -1,11 +1,3 @@
-const validCodes = [
-  100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300,
-  301, 302, 303, 304, 307, 308, 400, 401, 403, 404, 405, 406, 407, 408, 409,
-  410, 411, 412, 413, 414, 415, 416, 417, 421, 422, 423, 424, 425, 426, 428,
-  429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 511,
-];
-const validRangeCodes = ["1**", "2**", "3**", "4**", "5**"];
-
 export function channelRoutesCtrl ($scope, $timeout, Api, Alerting) {
   /*************************************************/
   /**   Default Channel Routes configurations     **/
@@ -331,14 +323,11 @@ export function channelRoutesCtrl ($scope, $timeout, Api, Alerting) {
     if (value !== '' && value !== undefined) {
       const codes = value.split(',')
       for (const code of codes) {
-        if (!code.includes("*")) {
-          if (!validCodes.includes(Number(code)) || isNaN(Number(code))) {
-            return `${code} not a valid status code!`
-          }
-        } else {
-          if (!validRangeCodes.includes(code)) {
-            return `${code} not a valid range of codes! valid options: ${validRangeCodes}`
-          }
+        const validRangeCodes = ["1**", "2**", "3**", "4**", "5**"];
+        if (Number(code) <= 100 || Number(code) >= 599) {
+          return `${code} not a valid status code!`
+        } else if (code.includes('*') && !validRangeCodes.includes(code)) {
+          return `${code} not a valid range of codes! valid options: ${validRangeCodes}`
         }
       }
     }
