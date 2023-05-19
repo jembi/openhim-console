eaa6bf328f426adf972a2369a798f26c4d73e5fa
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-05-19 18:12:18 +0100

    fix: minor fixes

--- infrastructure/google-cloud-run/docker-entrypoint.sh
+++ infrastructure/google-cloud-run/docker-entrypoint.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 
 cat config/default-env.json | envsubst $EXISTING_VARS | tee config/default.json
 
-nginx -g 'daemon off;'
+ls -l
+
+nginx -g "daemon off;"
