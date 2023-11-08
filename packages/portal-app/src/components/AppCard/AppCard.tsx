ed8384b17dabf570ec161bf1b04456a596364b7f
Mohamed Chedli <chedli@symbionix.co>
2023-11-08 11:44:58 +0100

    Update AppCard

--- packages/portal-app/src/components/AppCard/AppCard.tsx
+++ packages/portal-app/src/components/AppCard/AppCard.tsx
@@ -8,48 +8,48 @@ const AppCard = ({app}) => {
   return (
     <div>
       <Card sx={{maxWidth: '400px'}}>
-          <CardHeader
-            href={app.url}
-            avatar={
-              <Link href={app.url} target="_blank" rel="noopener noreferrer">
-                <Avatar variant="rounded" aria-label="application icon">
-                  {app.icon ? (
-                    <img src={app.icon} alt="application icon" width={24} />
-                  ) : (
-                    <AppsIcon fontSize="large" />
-                  )}
-                </Avatar>
-              </Link>
-            }
-            action={
-              <div>
-                {app.type == 'link' && (
-                  <IconButton
-                    aria-label="launch"
-                    href={app.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    size="large"
-                    color="primary"
-                    edge="end"
-                  >
-                    <LaunchIcon />
-                  </IconButton>
+        <CardHeader
+          href={app.url}
+          avatar={
+            <Link href={app.url}>
+              <Avatar variant="rounded" aria-label="application icon">
+                {app.icon ? (
+                  <img src={app.icon} alt="application icon" width={24} />
+                ) : (
+                  <AppsIcon fontSize="large" />
                 )}
-                <AppCardActionsMenu app={app} />
-              </div>
-            }
-            title={
-              <Link color="inherit" underline="hover" href={app.url}>
-                {app.name}
-              </Link>
-            }
-            subheader={
-              <Link color="inherit" underline="hover" href={app.url}>
-                {app.description}
-              </Link>
-            }
-          />
+              </Avatar>
+            </Link>
+          }
+          action={
+            <div>
+              {app.type == 'link' && (
+                <IconButton
+                  aria-label="launch"
+                  href={app.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  size="large"
+                  color="primary"
+                  edge="end"
+                >
+                  <LaunchIcon />
+                </IconButton>
+              )}
+              <AppCardActionsMenu app={app} />
+            </div>
+          }
+          title={
+            <Link color="inherit" underline="hover" href={app.url}>
+              {app.name}
+            </Link>
+          }
+          subheader={
+            <Link color="inherit" underline="hover" href={app.url}>
+              {app.description}
+            </Link>
+          }
+        />
       </Card>
     </div>
   )
