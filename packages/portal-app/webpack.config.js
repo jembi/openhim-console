d574d9ccd31a38e603af665bcc4a22fd1d9b7ccb
Mohamed Chedli <chedli@symbionix.co>
2023-11-08 11:52:50 +0100

    Add missing update in webpack.config from 757ce92

--- packages/portal-app/webpack.config.js
+++ packages/portal-app/webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+const DotenvPlugin = require('dotenv-webpack');
+
 const {merge} = require('webpack-merge')
 const singleSpaDefaults = require('webpack-config-single-spa-react-ts')
 
@@ -11,5 +14,9 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new webpack.EnvironmentPlugin(),
+      new DotenvPlugin()
+    ]
   })
 }
