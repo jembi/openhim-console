a7cddef6518c3f16455724a6076d8034eab18398
Mohamed Chedli <chedli@symbionix.co>
2023-11-09 10:01:40 +0100

    Rename API Base URL env var

--- packages/portal-app/src/utils/apiClient.ts
+++ packages/portal-app/src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//const API_URL = 'https://localhost:8080/'
-const API_URL = process.env.REACT_APP_API_BASE_URL;
+// set REACT_APP_OPENHIM_API_BASE_URL Env var to https://localhost:8080/ if running locally
+const API_URL = process.env.REACT_APP_OPENHIM_API_BASE_URL;
 
 const apiClient = axios.create({
   withCredentials: true,
