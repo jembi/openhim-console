914b4755c920a6891ebf880088de22e93a499fb2
Brett Onions <onionsllb@outlook.com>
2024-07-02 21:28:13 +0200

    fx: accessing client app on single spa

--- packages/clients-app/webpack.config.js
+++ packages/clients-app/webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack')
+const DotenvPlugin = require('dotenv-webpack')
+
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
@@ -7,9 +10,11 @@ module.exports = (webpackConfigEnv, argv) => {
     projectName: "clients-app",
     webpackConfigEnv,
     argv,
+    externals: [/^@jembi\/.+/]
   });
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    plugins: [new webpack.EnvironmentPlugin(), new DotenvPlugin()]
   });
 };
