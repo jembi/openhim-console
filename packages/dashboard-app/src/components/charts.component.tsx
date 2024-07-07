b66b2cdd81ea0929f9df2c5de670bf2868bc7871
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2024-07-07 19:08:51 +0200

    feat: use alert dialog for displaying errors.

--- packages/dashboard-app/src/components/charts.component.tsx
+++ packages/dashboard-app/src/components/charts.component.tsx
@@ -6,16 +6,26 @@ import {getTimeSeries} from '../services/api'
 import {TimeSeries, TimeSeriesScale} from '../types'
 import BasicFilter, {BasicFilterData} from './filters/basic.filter.component'
 import Loader from './ux/loader.component'
+import { AlertDialog, AlertDialogProps } from './ux/alert.dialog.component'
+
+type Alert = {
+  severity: AlertDialogProps['severity'];
+  isOpen: boolean;
+  title: string;
+  message: string;
+}
 
 export default function Charts() {
+  const now = new Date()
   const [timeSeries, setTimeSeries] = useState<TimeSeries[]>([])
   const [isFetchingTransactions, setIsFetchingTransactions] = useState(true)
   const [filterData, setFilterData] = useState<BasicFilterData>({
     period: TimeSeriesScale.day,
-    from: new Date(),
-    until: fns.add(new Date(), {hours: 23.99}),
+    from: now,
+    until: fns.add(now, {days: 1}),
     option: '1h'
   })
+  const [alert, setAlert] = useState<Alert>({ isOpen: false, title: '', message: '', severity: 'info' });
 
   const getFilteredTransactions = () => {
     setIsFetchingTransactions(true)
@@ -28,8 +38,9 @@ export default function Charts() {
         setTimeSeries(timeSeries)
       })
       .catch(err => {
-        setIsFetchingTransactions(false)
         console.error(err)
+        setIsFetchingTransactions(false)
+        setAlert({ isOpen: true, title: 'Error ',severity: 'error', message: 'Error loading data' });
       })
   }
 
@@ -46,44 +57,54 @@ export default function Charts() {
   }
 
   return (
-    <Grid container spacing={2} padding={2}>
-      <Grid item xs={12}>
-        <Typography variant="h3">Charts</Typography>
-        <p>An overview of recent transactions through your mediator.</p>
-      </Grid>
-      <Grid item xs={12}>
-        <BasicFilter value={filterData} onChange={onFilterChange} />
-      </Grid>
-      <Grid item>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <Card>
-              <TransactionLineChart
-                type="load"
-                data={timeSeries}
-                period={{
-                  from: filterData.from,
-                  until: filterData.until,
-                  type: filterData.period
-                }}
-              />
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card>
-              <TransactionLineChart
-                type="avgResponseTime"
-                data={timeSeries}
-                period={{
-                  from: filterData.from,
-                  until: filterData.until,
-                  type: filterData.period
-                }}
-              />
-            </Card>
+    <div>
+      <Grid container spacing={2} padding={2}>
+        <Grid item xs={12}>
+          <Typography variant="h3">Charts</Typography>
+          <p>An overview of recent transactions through your mediator.</p>
+        </Grid>
+        <Grid item xs={12}>
+          <BasicFilter value={filterData} onChange={onFilterChange} />
+        </Grid>
+        <Grid item>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Card>
+                <TransactionLineChart
+                  type="load"
+                  data={timeSeries}
+                  period={{
+                    from: filterData.from,
+                    until: filterData.until,
+                    type: filterData.period
+                  }}
+                />
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Card>
+                <TransactionLineChart
+                  type="avgResponseTime"
+                  data={timeSeries}
+                  period={{
+                    from: filterData.from,
+                    until: filterData.until,
+                    type: filterData.period
+                  }}
+                />
+              </Card>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+
+      <AlertDialog
+        severity={alert.severity}
+        title={alert.title}
+        message={alert.message}
+        open={alert.isOpen}
+        onClose={() => setAlert({ ...alert, isOpen: false })}
+      />
+    </div>
   )
 }
