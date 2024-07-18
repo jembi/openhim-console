ad97a7c2d22b10667390cb4b722a33e5234ed3ab
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2024-07-18 13:13:09 +0200

    feat: more updates to navbar.

--- packages/header-app/src/root.component.test.tsx
+++ packages/header-app/src/root.component.test.tsx
@@ -1,6 +1,6 @@
-import { render, fireEvent } from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import ButtonAppBar from './root.component'
-import { OpenHIMMenu } from '@jembi/openhim-sidebar'
+import {OpenHIMMenu} from '@jembi/openhim-sidebar'
 import React from 'react'
 
 jest.mock('@jembi/openhim-sidebar', () => ({
@@ -9,13 +9,13 @@ jest.mock('@jembi/openhim-sidebar', () => ({
 
 describe('ButtonAppBar component', () => {
   it('should render without errors', () => {
-    const { getByLabelText } = render(<ButtonAppBar />)
+    const {getByLabelText} = render(<ButtonAppBar />)
     expect(getByLabelText('open drawer')).toBeInTheDocument()
     expect(getByLabelText('account of current user')).toBeInTheDocument()
   })
 
   it('should open the menu on button click', () => {
-    const { getByLabelText, getByRole } = render(<ButtonAppBar />)
+    const {getByLabelText, getByRole} = render(<ButtonAppBar />)
     const menuButton = getByLabelText('account of current user')
     fireEvent.click(menuButton)
     const menu = getByRole('menu')
