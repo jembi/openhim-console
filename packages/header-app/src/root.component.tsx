a4192083303a0732a75121c5517a76c4bb64889e
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2024-07-18 14:34:53 +0200

    feat: remove sidebar app.

--- packages/header-app/src/root.component.tsx
+++ packages/header-app/src/root.component.tsx
@@ -1,12 +1,32 @@
 import theme from '@jembi/openhim-theme'
 import Box from '@mui/material/Box'
-import {ThemeProvider} from '@mui/material/styles'
+import {ThemeProvider, createTheme} from '@mui/material/styles'
 import OpenhimAppBar from './components/openhim.appbar.component'
 import {ConfirmationProvider} from './contexts/confirmation.context'
+import {green} from '@mui/material/colors'
+
+const customTheme = createTheme({
+  ...theme,
+  typography: {
+    // Tell MUI what's the font-size on the html element is.
+    // This is necessary as the legacy angular app uses bootstrap which
+    // changes the html font-size globally to 10px
+    htmlFontSize: 6,
+    fontSize: 6
+  },
+  palette: {
+    primary: {
+      main: green[700]
+    },
+    secondary: {
+      main: green['A100']
+    }
+  }
+})
 
 export default function Root() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={customTheme}>
       <ConfirmationProvider>
         <Box>
           <OpenhimAppBar />
