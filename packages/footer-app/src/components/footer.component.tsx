4c80516aa2eb9d34f7a46a0b4cb1a5aebcb71a8f
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2024-07-19 08:52:30 +0200

    feat: add openhim core version to the footer.

--- packages/footer-app/src/components/footer.component.tsx
+++ packages/footer-app/src/components/footer.component.tsx
@@ -1,5 +1,8 @@
 import {Box, Typography} from '@mui/material'
 import {makeStyles} from '@mui/styles'
+import { AboutResponse } from '../types'
+import React from 'react'
+import { getAbout } from '../services/api'
 
 const useStyles = makeStyles(theme => ({
   footer: {
@@ -10,12 +13,17 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export function Footer() {
+  const [about, setAbout] = React.useState<AboutResponse | null>(null)
   const classes = useStyles()
+
+  React.useEffect(() => {
+    getAbout().then(setAbout)
+  }, [])
 
   return (
     <Box className={classes.footer}>
       <Typography color={'grey'} fontSize={14} fontWeight={400} variant="body2">
-        OpenHIM {'v7.0.2'}
+        OpenHIM v{about?.currentCoreVersion ?? '???'}
         <br />
         Community-driven, powered by Jembi
       </Typography>
