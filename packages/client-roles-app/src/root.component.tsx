9bc43ff124d9f566114bee22f91074182b3ef9d4
Brett Onions <onionsllb@outlook.com>
2024-09-03 09:37:22 +0200

    fixing styling issues

--- packages/client-roles-app/src/root.component.tsx
+++ packages/client-roles-app/src/root.component.tsx
@@ -2,7 +2,10 @@ import {SnackbarProvider} from 'notistack'
 import {StrictMode, useState} from 'react'
 import {ThemeProvider} from '@emotion/react'
 import {ListRoles} from './pages/list-client-roles/list-client-roles'
-import {ClientRoleForm, loader as clientFormLoader} from './pages/client-role-form/client-role-form'
+import {
+  ClientRoleForm,
+  loader as clientFormLoader
+} from './pages/client-role-form/client-role-form'
 import {ClientRole} from './interface'
 import theme from '@jembi/openhim-theme'
 import './app.css'
@@ -33,36 +36,12 @@ export default function Root(props) {
   const [activePage, setActivePage] = useState<
     'list-roles' | 'client-role-form'
   >(defaultPage)
-  const [existingClientRole, setExistingClientRole] = useState<ClientRole>()
-
-  const returnToListRoles = () => {
-    setActivePage('list-roles')
-    window.history.pushState({}, '', '/#!/client-roles')
-  }
-  const addUserRole = () => {
-    setActivePage('client-role-form')
-    window.history.pushState({}, '', '/#!/client-roles/add')
-  }
-
-  const editUserRole = (clientRole: ClientRole) => {
-    setExistingClientRole(clientRole)
-    setActivePage('client-role-form')
-    window.history.pushState({}, '', '/#!/client-roles/edit/:roleName')
-  }
 
   return (
     <StrictMode>
       <ThemeProvider theme={theme}>
         <SnackbarProvider maxSnack={3} preventDuplicate>
-          <div
-            style={{
-              marginTop: '16px',
-              backgroundColor: '#F1F1F1',
-              height: '100vh'
-            }}
-          >
-            <RouterProvider router={router} />
-          </div>
+          <RouterProvider router={router} />
         </SnackbarProvider>
       </ThemeProvider>
     </StrictMode>
