ed241d0ae9f613f19cbdd5f3e8611089e877f5fc
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2024-09-13 08:23:14 +0200

    feat: add button to Alert components.

--- packages/channels-app/src/components/dialogs/alert.dialog.component.tsx
+++ packages/channels-app/src/components/dialogs/alert.dialog.component.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import Alert, { AlertColor } from '@mui/material/Alert';
-import Backdrop from '@mui/material/Backdrop';
-import AlertTitle from '@mui/material/AlertTitle';
-import Button from '@mui/material/Button';
+import React from 'react'
+import Alert, {AlertColor} from '@mui/material/Alert'
+import Backdrop from '@mui/material/Backdrop'
+import AlertTitle from '@mui/material/AlertTitle'
+import Button from '@mui/material/Button'
 
 export type AlertDialogProps = {
-  title?: string;
-  message: string;
-  open: boolean;
-  onClose: () => void;
-  severity?: AlertColor;
-};
+  title?: string
+  message: string
+  open: boolean
+  onClose: () => void
+  severity?: AlertColor
+}
 
 export function AlertDialog(props: AlertDialogProps) {
   return (
     <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      sx={{color: '#fff', zIndex: theme => theme.zIndex.drawer + 1}}
       onClick={props.onClose}
       open={props.open}
     >
-      <div style={{ minWidth: '500px' }}>
+      <div style={{minWidth: '500px'}}>
         <Alert
           variant="standard"
           severity={props.severity ?? 'info'}
           action={
-            <Button variant='contained' color={props.severity} size="small" onClick={props.onClose}>
+            <Button
+              variant="contained"
+              color={props.severity}
+              size="small"
+              onClick={props.onClose}
+            >
               OK
             </Button>
           }
@@ -34,5 +39,5 @@ export function AlertDialog(props: AlertDialogProps) {
         </Alert>
       </div>
     </Backdrop>
-  );
+  )
 }
