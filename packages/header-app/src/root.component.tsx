2825ec852423bdc1c2aa9fe65b6b5c5000a0a454
Brett Onions <onionsllb@outlook.com>
2024-09-30 15:49:20 +0200

    fx: Styling issues

--- packages/header-app/src/root.component.tsx
+++ packages/header-app/src/root.component.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box'
 import {ThemeProvider} from '@emotion/react'
 import OpenhimAppBar from './components/openhim.appbar.component'
 import {ConfirmationProvider} from './contexts/confirmation.context'
+import './header-styles.css'
 
 export default function Root() {
   return (
