5bc7d3b511961cd8f5d7f5fe3e61cc747498c173
Brett Onions <onionsllb@outlook.com>
2024-10-04 11:46:33 +0200

    touch ups

--- packages/clients-app/src/pages/clients-list/clients-list.tsx
+++ packages/clients-app/src/pages/clients-list/clients-list.tsx
@@ -53,22 +53,6 @@ const ClientsList = () => {
       })
   }, [])
 
-  const [clientToDelete, setClientToDelete] = useState<string | null>(null)
-  const handleClose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    if (e.currentTarget.id === 'confirm') {
-      deleteClient(clientToDelete)
-        .then(() => {})
-        .catch((error: any) => {
-          console.error(error)
-        })
-      const newClients = clients.filter(
-        client => client['_id'] !== clientToDelete
-      )
-      setClients(newClients)
-    }
-    setClientToDelete(null)
-  }
-
   const columns: GridColDef[] = [
     {field: 'clientID', headerName: 'ID', flex: 0.3},
     {field: 'name', headerName: 'Name', flex: 1},
@@ -95,15 +79,6 @@ const ClientsList = () => {
               '',
               `/#!/clients/edit/${params.row['_id']}`
             )
-          }}
-        />,
-        <GridActionsCellItem
-          icon={<DeleteIcon />}
-          id="delete"
-          label="Delete"
-          key="delete"
-          onClick={() => {
-            setClientToDelete(params.row['_id'])
           }}
         />
       ]
@@ -132,95 +107,75 @@ const ClientsList = () => {
   }
 
   return (
-    <>
-      <Dialog
-        open={!!clientToDelete}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {'Are you sure you want to delete this client?'}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Warning: This action cannot be undone. Once you delete a client, all
-            of its data will be permanently removed from the system.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button id="confirm" onClick={handleClose}>
-            Confirm
-          </Button>
-          <Button id="cancel" onClick={handleClose} autoFocus>
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Box
-        padding={1.5}
-        sx={{
-          backgroundColor: '#F1F1F1',
-          minHeight: 'calc(100vh - 119px - 10px)'
-        }}
-      >
-        <Grid container spacing={2} padding={2}>
-          <Grid item xs={12}>
-            <Typography variant="h4" gutterBottom>
-              Manage Clients
-            </Typography>
-            <Grid container>
-              <Grid item xs={11}>
-                <Typography variant="subtitle1" gutterBottom>
-                  Control client systems and their access roles. Add clients to
-                  enable their request routing and group them by roles for
-                  streamlined channel access management
-                </Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <a href={addingClient.toString()}>
-                  <Button variant="contained" startIcon={<AddIcon />}>
-                    Add
-                  </Button>
-                </a>
-              </Grid>
+    <Box
+      padding={1}
+      sx={{
+        backgroundColor: '#F1F1F1',
+        minHeight: 'calc(100vh - 119px - 10px)'
+      }}
+    >
+      <Grid container spacing={2} padding={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4" gutterBottom>
+            Manage Clients
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            sx={{
+              justifyContent: 'space-between',
+              alignItems: 'center'
+            }}
+          >
+            <Grid item>
+              <Typography variant="subtitle1" gutterBottom>
+                Control client systems and their access roles. Add clients to
+                enable their request routing and group them by roles for
+                streamlined channel access management
+              </Typography>
             </Grid>
-            <Divider sx={{marginTop: '10px', marginBottom: '30px'}} />
+            <Grid item>
+              <a href={addingClient.toString()}>
+                <Button variant="contained" startIcon={<AddIcon />}>
+                  Add
+                </Button>
+              </a>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Card>
-              <DataGrid
-                autoHeight
-                checkboxSelection
-                disableColumnMenu
-                disableRowSelectionOnClick
-                density="comfortable"
-                getRowId={row => row.clientID}
-                rows={clients}
-                columns={columns}
-                slots={{
-                  toolbar: GridToolbar,
-                  noRowsOverlay: CustomNoRowsOverlay
-                }}
-                slotProps={{
-                  toolbar: {
-                    showQuickFilter: true,
-                    printOptions: {disableToolbarButton: true},
-                    csvOptions: {disableToolbarButton: true}
-                  }
-                }}
-                initialState={{
-                  pagination: {
-                    paginationModel: {page: 0, pageSize: 10}
-                  }
-                }}
-                pageSizeOptions={[10, 25, 50]}
-              />
-            </Card>
-          </Grid>
+          <Divider sx={{marginTop: '10px', marginBottom: '30px'}} />
         </Grid>
-      </Box>
-    </>
+        <Grid item xs={12}>
+          <Card>
+            <DataGrid
+              autoHeight
+              disableColumnMenu
+              disableRowSelectionOnClick
+              density="comfortable"
+              getRowId={row => row.clientID}
+              rows={clients}
+              columns={columns}
+              slots={{
+                toolbar: GridToolbar,
+                noRowsOverlay: CustomNoRowsOverlay
+              }}
+              slotProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                  printOptions: {disableToolbarButton: true},
+                  csvOptions: {disableToolbarButton: true}
+                }
+              }}
+              initialState={{
+                pagination: {
+                  paginationModel: {page: 0, pageSize: 10}
+                }
+              }}
+              pageSizeOptions={[10, 25, 50]}
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
