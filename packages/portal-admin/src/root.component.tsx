43ca7ef38089398b4cb7f8dceb0a1faf0f156ba2
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2024-11-12 16:07:16 +0200

    feat: update Portal to use base page template.

--- packages/portal-admin/src/root.component.tsx
+++ packages/portal-admin/src/root.component.tsx
@@ -2,9 +2,8 @@ import {ThemeProvider} from '@mui/material/styles'
 import {Typography, Box, Paper} from '@mui/material'
 import {SnackbarProvider} from 'notistack'
 import theme from '@jembi/openhim-theme'
-
+import {BasePageTemplate} from '../../base-components'
 import {FormStateProvider} from './hooks/useFormType'
-
 import AppsDataGrid from '../src/components/AppsDataGrid'
 
 export default function PortalAdminRoot(props: any) {
@@ -12,17 +11,9 @@ export default function PortalAdminRoot(props: any) {
     <ThemeProvider theme={theme}>
       <FormStateProvider>
         <SnackbarProvider maxSnack={3} preventDuplicate>
-          <Box p={5}>
-            <Paper>
-              <Box p={2}>
-                <Typography variant="h4">Manage Apps</Typography>
-                <Typography variant="body2">
-                  Add and update all the Portal apps details and settings
-                </Typography>
-                <AppsDataGrid />
-              </Box>
-            </Paper>
-          </Box>
+          <BasePageTemplate title="Manage Apps" subtitle="Add and update all the Portal apps details and settings">
+            <AppsDataGrid />
+          </BasePageTemplate>
         </SnackbarProvider>
       </FormStateProvider>
     </ThemeProvider>
